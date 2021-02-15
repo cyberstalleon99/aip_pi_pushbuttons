a5bbb220af303493785ea809ee2b8c822ff7f9b0
cyberstalleon99 <clyde.khayad@gmail.com>
2021-02-15 15:02:29 +0800

    Update pushbuttons.py

--- pushbuttons.py
+++ pushbuttons.py
@@ -29,23 +29,31 @@ def init():
 	GPIO.setup(arrival_btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	GPIO.setup(departure_btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	
-	GPIO.add_event_detect(login_btn, GPIO.FALLING, bouncetime=200)
-	GPIO.add_event_detect(logout_btn, GPIO.FALLING, bouncetime=200)
-	GPIO.add_event_detect(arrival_btn, GPIO.FALLING, bouncetime=200)
-	GPIO.add_event_detect(departure_btn, GPIO.FALLING, bouncetime=200)
+	GPIO.add_event_detect(login_btn, GPIO.RISING,  bouncetime=200)
+	GPIO.add_event_detect(logout_btn, GPIO.RISING, bouncetime=200)
+	GPIO.add_event_detect(arrival_btn, GPIO.RISING, bouncetime=200)
+	GPIO.add_event_detect(departure_btn, GPIO.RISING, bouncetime=200)
 	
 def read(login_funcy, logout_funcy, arrival_funcy, departure_funcy, name):
 	if GPIO.event_detected(login_btn):
 		login_funcy(name)
+		#if GPIO.input(login_btn) == 1:
+		#	login_funcy(name)
 		
 	if GPIO.event_detected(logout_btn):
 		logout_funcy(name)
+		#if GPIO.input(logout_btn) == 1:
+		#	logout_funcy(name)
 		
 	if GPIO.event_detected(arrival_btn):
 		arrival_funcy(name)
+		#if GPIO.input(arrival_btn) == 1:
+		#	arrival_funcy(name)
 		
 	if GPIO.event_detected(departure_btn):
 		departure_funcy(name)
+		#if GPIO.input(departure_btn) == 1:
+		#	departure_funcy(name)
 
 #main()
 	
